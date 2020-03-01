3b12f3b20da62ffe7abca70c557ad7aecaaedfa3
KeisukeShimokawa <shimoke4869@gmail.com>
2020-03-01 05:19:44 +0000

    add file for downloading data

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+mkdir /opt/data/
+
+wget -P /opt/data/ https://www.dropbox.com/s/802qjuws26wp0ww/data-rev.zip
+unzip /opt/data/data-rev.zip -d /opt/data/
+rm /opt/data/data-rev.zip
+
+wget -P /opt/data/ https://www.dropbox.com/s/fha1x2o4zn3w2wb/data.zip
+unzip /opt/data/data.zip -d /opt/data/
+rm /opt/data/data.zip
