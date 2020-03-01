5aef04467e22baba863383c8f0d694fbf0192908
KeisukeShimokawa <33191285+KeisukeShimokawa@users.noreply.github.com>
2020-03-01 15:04:06 +0900

    Update download_data.sh
    fix csv file name

--- download_data.sh
+++ download_data.sh
@@ -4,6 +4,6 @@ wget -P /opt/data/ https://www.dropbox.com/s/802qjuws26wp0ww/data-rev.zip
 unzip /opt/data/data-rev.zip -d /opt/data/
 rm /opt/data/data-rev.zip
 
-wget -P /opt/data/ https://www.dropbox.com/s/fha1x2o4zn3w2wb/data.zip
+wget -P /opt/data/ https://www.dropbox.com/s/g0bqpgstzv2ojv7/data.zip
 unzip /opt/data/data.zip -d /opt/data/
 rm /opt/data/data.zip
