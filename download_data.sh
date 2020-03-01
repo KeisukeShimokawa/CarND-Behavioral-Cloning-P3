72503326e0876342ac89a491ee2260860adcc64d
KeisukeShimokawa <shimoke4869@gmail.com>
2020-03-01 10:06:35 +0000

    add new dataset

--- download_data.sh
+++ download_data.sh
@@ -1,9 +1,22 @@
+rm -rf ./data
 mkdir /opt/data/
 
 wget -P /opt/data/ https://www.dropbox.com/s/802qjuws26wp0ww/data-rev.zip
-unzip /opt/data/data-rev.zip -d /opt/data/
+unzip -o /opt/data/data-rev.zip -d /opt/data/
 rm /opt/data/data-rev.zip
 
 wget -P /opt/data/ https://www.dropbox.com/s/g0bqpgstzv2ojv7/data.zip
-unzip /opt/data/data.zip -d /opt/data/
+unzip -o /opt/data/data.zip -d /opt/data/
 rm /opt/data/data.zip
+
+wget -P /opt/data/ https://www.dropbox.com/s/tz37aitrl2umibu/data-normal1.zip
+unzip -o /opt/data/data-normal1.zip -d /opt/data/
+rm /opt/data/data-normal1.zip
+
+wget -P /opt/data/ https://www.dropbox.com/s/6c45r42p6imoeqv/data-normal-rev1.zip
+unzip -o /opt/data/data-normal-rev1.zip -d /opt/data/
+rm /opt/data/data-normal-rev1.zip
+
+wget -P /opt/data/ https://www.dropbox.com/s/o27x0nfu2klndd0/data-left.zip
+unzip -o /opt/data/data-left.zip -d /opt/data/
+rm /opt/data/data-left.zip
